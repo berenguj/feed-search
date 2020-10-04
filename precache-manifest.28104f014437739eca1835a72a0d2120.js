cfd1e07cbf867cc775f204e322a555a605d35033
Jada <berenguj@uci.edu>
2020-10-04 16:40:03 -0700

    Updates

---
+++ precache-manifest.28104f014437739eca1835a72a0d2120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5947b9ae19fd04b0420bfc0b6a874625",
+    "url": "/feed-search/index.html"
+  },
+  {
+    "revision": "7f771077aa0b43160c3d",
+    "url": "/feed-search/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "357ae2a96f21b01f8db5",
+    "url": "/feed-search/static/js/2.0f104b28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feed-search/static/js/2.0f104b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f771077aa0b43160c3d",
+    "url": "/feed-search/static/js/main.e8a3005e.chunk.js"
+  },
+  {
+    "revision": "29e03f3b7ad87c86a446",
+    "url": "/feed-search/static/js/runtime-main.0895e108.js"
+  }
+]);
